4c565a702932a02ea286c6a21e6ba51e3c33f003
yp-ww <rororo3434@yahoo.co.jp>
2024-05-06 22:04:45 +0900

    Create .gitignore and test.cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello World!!" << endl;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;    
+    return 0;
+}
