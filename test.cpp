d893c036c26d7f591035387558f651d921f342d6
yp-ww <rororo3434@yahoo.co.jp>
2024-05-06 22:42:48 +0900

    Update test files

--- test.cpp
+++ test.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
+
+using mint = atcoder::modint;
 
 int main() {
     cout << "Hello World!!" << endl;
     
     int a, b;
     cin >> a >> b;
-    cout << "a+b: " << a + b << endl;    
+    cout << "a+b: " << a + b << endl;
+
+    mint::set_mod(3);
+    mint c = 5;
+    cout << "c: " << c.val() << endl;
     return 0;
 }
---
+++ test.py
@@ -0,0 +1,7 @@
+import sys
+sys.setrecursionlimit(1000000)
+
+print("Hello World!")
+
+a = int(input())
+print("a:", a)
